cd51ec55859250e52deac46470bb2cf5e267d327
Kerem İlhan <99633775+keremilhan@users.noreply.github.com>
2025-09-19 14:26:41 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,9 +4,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Card Project Backend")
 
+origins = [
+    "https://card-project-alpha.vercel.app",
+    "http://localhost:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
